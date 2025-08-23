6fd98572e0685efaefd04ba2fbdcae1ef52460ae
VyomBajaj <vyombajaj04@gmail.com>
2025-08-23 21:59:17 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <>
+    <div className="bg-slate-300">
+    <h1 className="text-3xl font-bold underline">
+      Welcome to Legal Doc
+    </h1>
+    </div>
+    </>
+  )
+}

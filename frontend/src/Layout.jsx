b5175e93327a3d49a528de795a41592f57c96d8d
Apratap01 <youremail@example.com>
2025-09-05 02:36:49 +0530

    feat: add Summary page and integrate routing for document summaries

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -5,9 +5,9 @@ import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
-    <div className="flex flex-col min-h-screen bg-gradient-to-r from-[#141414] to-[#050448]">
+    <div className="flex flex-col min-h-screen bg-gradient-to-r from-[#03070e] to-[#050448]">
       <Navbar />
-      <main className="flex-1">
+      <main className="flex-1 ">
         <Outlet /> {/* This is where Login, Signup, Home etc. will render */}
       </main>
       <Footer />
--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,2 +1,3 @@
 export const USER_API_ENDPOINT = "http://localhost:1818/api/user"
 export const DOCS_API_ENDPOINT = "http://localhost:1818/api/docs"
+export const SUMMARY_API_ENDPOINT = "http://localhost:1818/api/summary"

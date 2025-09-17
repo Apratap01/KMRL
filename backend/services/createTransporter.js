0b691bcfcba2a84eb276ed68f6bd0ca738b37a19
VyomBajaj <vyombajaj04@gmail.com>
2025-09-17 19:32:35 +0530

    Changes

--- backend/services/createTransporter.js
+++ backend/services/createTransporter.js
@@ -2,7 +2,8 @@ import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
 dotenv.config()
 export const createTransporter = async () => {
-    if (process.env.NODE_ENV=="production") {
+    const environment = process.env.NODE_ENV == 'development'
+    if (!environment) {
         // In production, use Gmail, Mailtrap, etc.
         return nodemailer.createTransport({
             service: "gmail",
@@ -10,6 +11,7 @@ export const createTransporter = async () => {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
             },
+            secure:true,
         });
     } else {
         // In development, use Ethereal test SMTP
